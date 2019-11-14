c3c16416d7e09ccc5c6b5f20fd5bfc9bdaf7c457
youvovk <wer3wollf@gmail.com>
2019-11-14 21:24:17 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -6,42 +6,40 @@ const fs = require('fs');
 const app = express();
 const port = 5000;
 
-let movies = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+let moviesData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 app.get('/movies', (req, res) => {
-    const allMovies = [...movies].sort((a, b) => Number(b.year) - Number(a.year));
+    const movies = [...moviesData].sort((a, b) => Number(b.year) - Number(a.year));
 
-    res.json(allMovies);
+    res.json(movies);
 });
 
 app.get('/movies/titles', (req, res) => {
-    let titles = [...movies];
+    let titles = [...moviesData];
 
     if (req.query.year) {
         titles = titles.filter(({ year }) => year === req.query.year);
 
         if (!titles.length) {
-            res.status(404).send("Movie title is not found");
+            res.status(200).send("Movie title is not found");
         }
     }
 
 
-    titles = titles.map(({ title }) => title).sort().reduce((acum, title) => acum + ` <h2>${title}</h2>`, '');
+    titles = titles.map(({ title }) => title).sort().join('\n');
 
     res.send(titles);
 });
 
-app.get('/movies/:id', (req, res) => {
-    const foundMoviesId = movies.find(({ id }) => id === req.params.id);
+app.get('/movies/:movieId', (req, res) => {
+    const foundMoviesId = moviesData.find(({ id }) => id === req.params.id);
 
     if (foundMoviesId) {
         return res.json(foundMoviesId);
-    } else {
-        res.status(404).send("Oops, something went wrong");
-    }
+    } 
+
+    return  res.status(404).send("Oops, something went wrong");
+    
 });
 
-app.use(express.static('build'))
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-console.log('Listening...')
